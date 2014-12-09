1ee439eea85bad768b4e658165ad7f99ab138b40
nick <nick@nxk.me>
2014-12-09 13:40:52 +1100

    give users UUIDs if they don't already have one

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra/base'
+require 'sequel'
+require 'securerandom'
 
 module MMOWriter
   NAME = 'MMOWriter'
   VERSION = '0.0.1'
+end
 
+# relative requires here
+
+module MMOWriter
   class App < Sinatra::Base
     set :root, File.dirname(__FILE__)
+    
+    before '/*' do # Before everything
+      # If user does not have a UUID, give them one
+      response.set_cookie('u', SecureRandom.uuid) if request.cookies['u'].nil?
+    end
     
     get '/' do
       erb :home, :layout => :global
