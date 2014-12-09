fef4d43425d3f8d3be65eddbcd29c5d91ff4c958
nick <nick@nxk.me>
2014-12-09 20:52:21 +1100

    change /w/:id/votes output to AJAX buttons instead of JSON

--- routes/write_story.rb
+++ routes/write_story.rb
@@ -9,19 +9,34 @@ module MMOWriter::Routes
         story = MMOWriter::Story[params[:id]]
         votes = {}
         
+        return 'no votes yet' if story.votes.length < 1
+        
         story.votes.each do |vote|
           vote_hash = {:type => vote.action_type, :metadata => vote.action_metadata}
           votes[vote_hash] = 0 if votes[vote_hash].nil?
           votes[vote_hash] += 1
         end
         popular_votes = votes.sort_by {|_, v| v}
-        my_vote = story.votes.find{|v| v.uuid == request.cookies['u']}
-        if !my_vote.nil?
-          my_vote = [{:type => my_vote.action_type, :metadata => my_vote.action_metadata}, votes[{:type => my_vote.action_type, :metadata => my_vote.action_metadata}]]
-        end
         popular_votes = popular_votes[-5 .. -1] if popular_votes.length > 5
         
-        JSON.generate({'popular_votes' => popular_votes, 'my_vote' => my_vote})
+        out = ''
+        popular_votes.reverse.each do |vote|
+          text = vote[0][:metadata]
+          color_string = ''
+          
+          case vote[0][:type]
+          when 'special_start_char', 'special_end_char'
+            color_string = '_green'
+          when 'paragraph'
+            text = 'new paragraph'
+            color_string = '_green'
+          when 'story_end'
+            text = 'story end'
+            color_string = '_red'
+          end
+          out += "<button type=\"button\" class=\"user_input_button#{color_string}\"> #{vote[1]} vote#{'s' if vote[1] != 1} | " + text + "</button> "
+        end        
+        out
       end
     end
   end
