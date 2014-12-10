841b3ea58b35a27fc1a1880f81eb81e8b62bae43
nick <nick@nxk.me>
2014-12-10 15:21:37 +1100

    move more data into periodic ajax call

--- routes/write_story.rb
+++ routes/write_story.rb
@@ -6,38 +6,44 @@ module MMOWriter::Routes
         erb :write_story, :locals => {:story => MMOWriter::Story[params[:id]]}, :layout => :global
       end
       
-      app.get '/w/:id/votes' do
+      app.get '/w/:id/ajax_on_clock' do
+        content_type :json
+      
         story = MMOWriter::Story[params[:id]]
-        votes = {}
+        html_buttons = ''
         
-        return 'no votes yet' if story.votes.length < 1
-        
-        story.votes.each do |vote|
-          vote_hash = {:type => vote.action_type, :metadata => vote.action_metadata}
-          votes[vote_hash] = 0 if votes[vote_hash].nil?
-          votes[vote_hash] += 1
-        end
-        popular_votes = votes.sort_by {|_, v| v}
-        popular_votes = popular_votes[-5 .. -1] if popular_votes.length > 5
-        
-        out = ''
-        popular_votes.reverse.each do |vote|
-          text = vote[0][:metadata]
-          color_string = ''
-          
-          case vote[0][:type]
-          when 'special_start_char', 'special_end_char'
-            color_string = '_green'
-          when 'paragraph'
-            text = 'new paragraph'
-            color_string = '_green'
-          when 'story_end'
-            text = 'story end'
-            color_string = '_red'
+        if story.votes.length < 1
+          html_buttons = 'no votes yet'
+        else
+          votes = {}
+          story.votes.each do |vote|
+            vote_hash = {:type => vote.action_type, :metadata => vote.action_metadata}
+            votes[vote_hash] = 0 if votes[vote_hash].nil?
+            votes[vote_hash] += 1
           end
-          out += "<button type=\"button\" class=\"user_input_button#{color_string}\" onclick=\"postVote('#{vote[0][:type]}', '#{vote[0][:metadata]}')\"> #{vote[1]} vote#{'s' if vote[1] != 1} | " + text + "</button> "
-        end        
-        out
+          popular_votes = votes.sort_by {|_, v| v}
+          popular_votes = popular_votes[-5 .. -1] if popular_votes.length > 5
+          
+          
+          popular_votes.reverse.each do |vote|
+            text = vote[0][:metadata]
+            color_string = ''
+            
+            case vote[0][:type]
+            when 'special_start_char', 'special_end_char'
+              color_string = '_green'
+            when 'paragraph'
+              text = 'new paragraph'
+              color_string = '_green'
+            when 'story_end'
+              text = 'story end'
+              color_string = '_red'
+            end
+            html_buttons += "<button type=\"button\" class=\"user_input_button#{color_string}\" onclick=\"postVote('#{vote[0][:type]}', '#{vote[0][:metadata]}')\"> #{vote[1]} vote#{'s' if vote[1] != 1} | " + text + "</button> "
+          end
+        end
+        
+        JSON.generate({'h' => html_buttons, 'c' => (MMOWriter::VOTE_TIMEOUT - ((Time.now.to_i - story.date_created) % MMOWriter::VOTE_TIMEOUT)), 's' => story.completed})
       end
       
       app.post '/w/:id' do
